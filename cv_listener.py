63935c231a24c6cceca22f1e86780265e716b672
fatihselimyakar <fatihselimyakar@gmail.com>
2019-12-06 07:57:58 -0800

    Improvements added bugs fixed.

--- cv_listener.py
+++ cv_listener.py
@@ -9,14 +9,25 @@ class ServerHandler(http.server.CGIHTTPRequestHandler):
     def do_POST(self):
       content_len = int(self.headers.get('Content-Length'))
       post_body = self.rfile.read(content_len)
-      print (post_body)
-      #json_string = json.dumps(post_body)
-      #print (json_string)
-      
+      #print (post_body)
+
+      json_string = post_body.decode('utf8').replace("'", '"')
+      #print(json_string)
+
+      #creates json object
+      data = json.loads(json_string)
+      #prints json object's properties part(like array accessing)
+      print(data["properties"]["failure"])
+      #converts json object to string
+      s = json.dumps(data, indent=4, sort_keys=True)
+      #prints the json file
+      print (s)
+
       self.send_response(200)
-      self.send_header("Content-type", "")
+      self.send_header("State", "Delivery Confirmed")
       self.end_headers()
-      #self.wfile.write(json.dumps("njcs"))
+      #sends response 
+      self.wfile.write(post_body)
 
 Handler = ServerHandler
 
--- cv_request.py
+++ cv_request.py
@@ -7,7 +7,7 @@ data = json.load(jsonFile)
 print (data)
 
 # sending post request and saving response as response object 
-r = requests.post(url = 'http://localhost:8081', data = data) 
+r = requests.post(url = 'http://localhost:8081', data = json.dumps(data)) 
 # extracting response text 
-pastebin_url = r.text 
-print("\nThe pastebin URL is:%s"%pastebin_url)
+sent_json = r.headers
+print("\nResponse:%s"%sent_json)
