e989b9fbb69df1109fdbf9cc6e892d8936f64026
fatihselimyakar <fatihselimyakar@gmail.com>
2019-12-06 09:09:25 -0800

    rearranged the cv_listener.py

--- cv_listener.py
+++ cv_listener.py
@@ -2,32 +2,27 @@ import json
 import http.server
 import socketserver
 
+#json.dumps/dump -> creates json file
+#json.loads/load -> parses json file
 PORT = 8081
 
 class ServerHandler(http.server.CGIHTTPRequestHandler):
 
     def do_POST(self):
       content_len = int(self.headers.get('Content-Length'))
-      post_body = self.rfile.read(content_len)
-      #print (post_body)
-
-      json_string = post_body.decode('utf8').replace("'", '"')
-      #print(json_string)
-
-      #creates json object
-      data = json.loads(json_string)
-      #prints json object's properties part(like array accessing)
+      #bytewise json file
+      json_body = self.rfile.read(content_len)
+      print(json_body)
+      #parses json file
+      data = json.loads(json_body)
+      #prints json object's properties,failure part(like array accessing)
       print(data["properties"]["failure"])
-      #converts json object to string
-      s = json.dumps(data, indent=4, sort_keys=True)
-      #prints the json file
-      print (s)
 
       self.send_response(200)
       self.send_header("State", "Delivery Confirmed")
       self.end_headers()
-      #sends response 
-      self.wfile.write(post_body)
+      #sends response itself
+      self.wfile.write(json_body)
 
 Handler = ServerHandler
 
