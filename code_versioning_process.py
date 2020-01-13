1c3e659b1d244a2e09c795a46bb7d1c33fc0dc19
Fatih Selim Yakar <32717115+fatihselimyakar@users.noreply.github.com>
2020-01-13 19:31:44 +0300

    Updated

--- code_versioning_process.py
+++ code_versioning_process.py
@@ -7,6 +7,35 @@ logging.basicConfig(format='%(asctime)s %(message)s',
                 filename='code_versioning.log',
                 filemode='w',
                 level=logging.INFO)
+                
+def json_load_byteified(file_handle):
+    return _byteify(
+        json.load(file_handle, object_hook=_byteify),
+        ignore_dicts=True
+    )
+
+def json_loads_byteified(json_text):
+    return _byteify(
+        json.loads(json_text, object_hook=_byteify),
+        ignore_dicts=True
+    )
+
+def _byteify(data, ignore_dicts = False):
+    # if this is a unicode string, return its string representation
+    if isinstance(data, unicode):
+        return data.encode('utf-8')
+    # if this is a list of values, return list of byteified values
+    if isinstance(data, list):
+        return [ _byteify(item, ignore_dicts=True) for item in data ]
+    # if this is a dictionary, return dictionary of byteified keys and values
+    # but only if we haven't already byteified it
+    if isinstance(data, dict) and not ignore_dicts:
+        return {
+            _byteify(key, ignore_dicts=True): _byteify(value, ignore_dicts=True)
+            for key, value in data.iteritems()
+        }
+    # if it's anything else, return it in its original form
+    return data
 
 class code_versioning:  
         
@@ -18,7 +47,7 @@ class code_versioning:
     def __init__(self, jsonFile):      
         logging.info('Object created')
         #parametre olarak gelen json fileini load(islenebilecek hale getirir) eder.
-        self.comingJson = json.load(jsonFile)
+        self.comingJson = json_loads_byteified(jsonFile)
     
     #directoryde bir json file olusturur(asagi kisimlarda hem request olarak gonderilcek hem okunarak bilgiler alincak)
     def createJsonFileInDirectory(self):
@@ -157,15 +186,14 @@ class code_versioning:
 
     
 # Driver Code  
-def main():
+def main(jfile):
     #alttaki json datasi mule tarafindan direk verilecek.
     #ilk calismasi icin gereken json file
-    jfile = open(r'/home/fatihselimyakar/Desktop/CSE343_Code_Versioning/initial_request.json', 'r')
+    #jfile = open(r'/home/fatihselimyakar/Desktop/CSE343_Code_Versioning/initial_request.json', 'r')
     #jfile = open(r'/home/fatihselimyakar/Desktop/CSE343_Code_Versioning/process_file.json', 'r')
     cv_obj=code_versioning(jfile)
     cv_obj.parseJson()
 
- 
-if __name__== "__main__":
-  main()
+
+main(jfile)
 
